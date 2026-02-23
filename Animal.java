8be88352cbb2ebb24b16cfa1296d230f71cb509e
marlinRivas7 <marlinrivas042@gmail.com>
2026-02-23 00:13:18 -0800

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,14 @@
+
+    public class Animal {
+        public static void main(String [] args) {
+
+            Cat myCat = new Cat();
+            myCat.meow();
+            myCat.name = "Stella";
+            myCat.age = 8;
+
+            System.out.println(Cat.MAX_LIVES);
+            System.out.println(Cat.getCatCount());
+
+        }
+    }
